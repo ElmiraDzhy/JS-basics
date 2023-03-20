feabd0c7364a7be62bd4f45e9006ee67c84b7dc0
ElyaDzhy <elmira.freshcode@gmail.com>
2023-03-20 09:36:57 +0200

    Add resolve of factorial and pow func

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 'use strict';
+
+function myPow (number, power) {
+    let result = 1;
+    for(let  i = 0; i < power; i++){
+        result *= number;
+    }
+    return result;
+}
+
+function factorial(number){
+    let result = 1;
+    if(number === 0 || number === 1){
+        return 1;
+    }
+    if(number < 0){
+        return 'Error';
+    }
+    for(let i = 1; i <= number; i++){
+        result *= i;
+    }
+    return result;
+
+}
