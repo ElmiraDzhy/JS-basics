c6826bf0316b0c73ae449bae0c8da95ee28b3b3a
ElyaDzhy <elmira.freshcode@gmail.com>
2023-03-20 11:57:46 +0200

    Homework tasks resolve

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 'use strict';
 
 
-const PROTOCOL = 'http://';
+//homework
 
-const SERVER_IP = 'domain.com';
-
-const PORT = 3000;
-
-function getFullUrl(path=''){
-    return `${PROTOCOL}${SERVER_IP}:${PORT}/${path}`
+function multiplyTable (num) {
+    let result = 1;
+    for(let i = 1; i <= num; i++){
+        result = 2 * i;
+        console.log(`2 * ${i} = ${result}`)
+    }
 }
+
+
+//
+
+const user = {
+    name: 'Name',
+    lastName: 'LastName',
+    age: 28,
+}
+const user1 = {
+    name: 'Name1',
+    lastName: 'LastName1',
+    age: 23,
+}
+
+const user2 = {
+    name: 'Name2',
+    lastName: 'LastName2',
+    age: 13,
+}
+
+
+function getBanned (obj) {
+    obj.banned = true;
+    return obj.banned;
+    
+}
+
+console.log(getBanned(user));
+console.log(getBanned(user1));
+console.log(getBanned(user2));
+
 
