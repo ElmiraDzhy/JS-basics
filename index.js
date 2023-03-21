a8a930180ffae92518f66fdd007dede0a749a480
ElyaDzhy <elmira.freshcode@gmail.com>
2023-03-21 09:59:37 +0200

    Object entity equals

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 'use strict';
-let name = 'Alex';
-let age = 15;
-
-const user = {
-    age,
-    name,
-
-}
 
 const car = {
     brand: 'Audi',
@@ -16,11 +8,29 @@ const car = {
 
 
 }
-// prop in obj
 
-console.log('color' in car);
-console.log('brand' in car);
+const car2 = {
+    brand: 'Audi',
+    type: 'sedan',
+    volume: 1.6,
+    capasity: 5,
 
 
+}
 
+for (const key in car){
+    console.log(`${key}: ${car[key]}`);
+}
+
+function isObjectEquals(obj1, obj2){
+
+    for (const key in obj1){
+        if(obj1[key] !== obj2[key]){
+            return false;
+        } 
+    }
+    return true;
+}
+
+console.log(isObjectEquals(car, car2));
 
