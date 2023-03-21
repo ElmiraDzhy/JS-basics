8dfeaf5ab166d75daab67ef281e5aead15464061
ElyaDzhy <elmira.freshcode@gmail.com>
2023-03-21 11:51:22 +0200

    Add Car task resolve

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
 'use strict';
 
 
-const cat2 = {
-  name: 'barsik',
-  age: 3,
-  color: 'grey',
-}
-const cat3 = {
-  name: 'gold',
-  age: 2,
-  color: 'black',
-}
-const cat4 = {
-  name: 'mukr',
-  age: 4,
-  color: 'red',
-}
-
-
 function Cat(name, color, age){
   this.name = name;
   this.color = color;
@@ -28,6 +11,64 @@ function Cat(name, color, age){
   }
 }
 
-const blackCat = new Cat('Kitty', 3);
 
-const cat = new Cat('mukr', 5, 'red');
+const cat = new Cat('murzik', 5, 'red');
+cat.askForFood = function(){
+  return `${this.name} wanna eat!!!`;
+}
+
+function User(firstName, lastName, age, email){
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.email = email;
+
+
+  this.sayHello = function(){
+    return `${this.firstName} say hello!`;
+  }
+
+  this.checkMail = function(){
+    return this.email;
+  }
+
+}
+
+const user = new User('John', "Doe", 23, 'john.mail.com');
+const user2 = new User('Jane', "Doe", 22, 'jane.mail.com');
+
+function Car (brand, maxSpeed, speed) {
+  this.brand = brand;
+  this.maxSpeed = maxSpeed;
+  this.speed = speed;
+
+  this.accelerate = function(num){
+    if(this.speed+num <= this.maxSpeed){
+      this.speed+=num ;
+    }else{
+      this.speed = this.maxSpeed;
+    }
+      return  this.speed;
+  }
+
+  this.deaccelerate = function(num){
+    if(this.speed-num >= 0 ){
+      this.speed-=num;
+    }else{
+      this.speed = 0;
+    }
+      return  this.speed;
+  }
+
+
+  this.stop = function(){
+    this.speed = 0;
+    return this.speed;
+  }
+}
+
+const car = new Car('Toyota', 200, 60 );
+console.log(`car.speed = ${car.speed}`);
+console.log(`car.accelerate(40) = ${car.accelerate(40)}`);
+console.log(`car.accelerate(20) = ${car.accelerate(20)}`);
+console.log(`car.stop() = ${car.stop()}`);
