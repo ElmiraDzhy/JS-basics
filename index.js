96006098cf8b06b897a25b2276f284ba9bca3799
ElyaDzhy <elmira.freshcode@gmail.com>
2023-03-22 09:43:07 +0200

    Biggest num task resolve

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
 'use strict';
 
 
-const userPhone1 = 380986754322;
-const userPhone2 = 380986764531;
-const userPhone3 = 380986789002;
-const userPhone4 = 380988674932;
-const userPhone5 = 380976589002;
-const userPhone6 = 380678089002;
-const userPhone7 = 380986700002;
-const userPhone8 = 380986234002;
+const numbers = new Array(1,60,4,8,9,70);
 
-
-const usersPhones = [userPhone1, userPhone2, userPhone3, userPhone4, userPhone5,userPhone6,userPhone7,userPhone8];
-
-
-for (let index = 0; index < usersPhones.length; index++) {
-  console.log(`${index}: ${usersPhones[index]}`);
-}
-
-
-const numbers = new Array(1,4,8,9);
-
-let sum = 0;
+let biggestNum = 0;
 for (let index = 0; index < numbers.length; index++) {
-  sum += numbers[index];
-  
+  if(numbers[index] > biggestNum){
+    biggestNum = numbers[index];
+  }
+
 }
-console.log(sum);
+console.log(biggestNum);
