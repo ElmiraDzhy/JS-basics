8f48b89c9229ba2957397b003f39012489a191af
ElyaDzhy <elmira.freshcode@gmail.com>
2023-03-22 10:24:10 +0200

    Add arraya task resolve(Mean and even)

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 'use strict';
 
-
-const numbers = new Array(1,60,4,8,9,70);
-
-let biggestNum = 0;
-for (let index = 0; index < numbers.length; index++) {
-  if(numbers[index] > biggestNum){
-    biggestNum = numbers[index];
+function getArithmeticMeanOf(array){
+  let sum = 0;
+  for (let index = 0; index < array.length; index++) {
+   sum += array[index];
   }
 
+  return sum/array.length;
 }
-console.log(biggestNum);
+
+function sumAllEven(array) {
+  let sum = 0;
+  for (let index = 0; index < array.length; index+=2) {
+      sum+= array[index];
+   }
+  return sum;
+}
+
+
+const numbers = new Array(1, 60, 4, 8, 9, 70,100);
+
+console.log(getArithmeticMeanOf(numbers));
+console.log(sumAllEven(numbers));
+
+
