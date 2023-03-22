7a452a69b44cca441441b04eb700dd8653e94eef
ElyaDzhy <elmira.freshcode@gmail.com>
2023-03-22 09:32:57 +0200

    Into in arrays

--- index.js
+++ index.js
@@ -1,52 +1,29 @@
 'use strict';
 
-//homework
 
-function Worker (firstName, lastName, rate, days) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.rate = rate;
-  this.days = days;
-  
+const userPhone1 = 380986754322;
+const userPhone2 = 380986764531;
+const userPhone3 = 380986789002;
+const userPhone4 = 380988674932;
+const userPhone5 = 380976589002;
+const userPhone6 = 380678089002;
+const userPhone7 = 380986700002;
+const userPhone8 = 380986234002;
 
-  this.getSalary = function(){
-    if(this.rate < 0){
-      this.rate = 0;
-      return new RangeError('RATE CAN NOT BE LESS THAN 0');
-    }
-    if(this.days < 0){
-      this.days = 0;
-      return new RangeError('DAYS CAN NOT BE LESS THAN 0');
 
-    }
-    return this.rate*this.days;
-  }
-  
+const usersPhones = [userPhone1, userPhone2, userPhone3, userPhone4, userPhone5,userPhone6,userPhone7,userPhone8];
+
+
+for (let index = 0; index < usersPhones.length; index++) {
+  console.log(`${index}: ${usersPhones[index]}`);
 }
 
-function Country (name, area, population) {
-  this.name = name;
-  this.area = area;
-  this.population = population;
 
-  this.populationDensity = function(){
+const numbers = new Array(1,4,8,9);
 
-    if(this.population < 0){
-      this.population = 0;
-      return new RangeError('POPULATION CAN NOT BE LESS THAN 0');
-    }
-    if(this.area < 0){
-      this.area = 0;
-      return new RangeError('AREA CAN NOT BE LESS THAN 0');
-
-    }
-
-    return this.population/this.area;
-  }
+let sum = 0;
+for (let index = 0; index < numbers.length; index++) {
+  sum += numbers[index];
+  
 }
-
-const worker = new Worker('Name', 'Surname', 50, 12);
-console.log(worker.getSalary());
-
-const country = new Country('USA', 5000000, 10000);
-console.log(country.populationDensity());
+console.log(sum);
