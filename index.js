b3b492cad4495889db40ae5ee679609e3ddd8fcf
ElyaDzhy <elmira.freshcode@gmail.com>
2023-03-22 11:59:40 +0200

    Add sort method example

--- index.js
+++ index.js
@@ -3,9 +3,29 @@
 
 const array = [1,2,3,4,5,6,7,8,9];
 
-const arrayEven = array.filter( (element) =>{
-      return element%2 === 0;
-} );
+const users = [
+  {
+    name: 'test',
+    age: 22,
+  },
+  {
+    name: 'name',
+    age: 12,
+  },
+  {
+    name: 'value',
+    age: 20,
+  },
+]
 
-console.log(arrayEven);
+array.sort((prev, next) => {
+     return -1;
+});
 
+console.log(array);
+
+users.sort( (prev,next) => {
+  return prev.age > next.age  ? 1 : -1;
+} )
+
+console.log(users);
