bdff1ec5a50471b6d6e5ba5513deb8e029ad1d64
ElyaDzhy <elmira.freshcode@gmail.com>
2023-03-27 11:19:25 +0300

    Classes example

--- index.js
+++ index.js
@@ -1,39 +1,79 @@
 'use strict';
 
-// ERRORS
-//  EXEPTIONS
+// CLASSES
 
 
-/**
- * 
- * @param {Number} num  - number that will power
- * @param {Number} exp - exponent
- * @throws {TypeError} - if args  is incorrect 
- * @throws {RangeError} - if exp < 0
- * @returns {Number}
- */
-function pow(num, exp){
-let result = num;
-
-  if(typeof num !== 'number' && typeof exp !== 'number'){
-
-    throw new TypeError('Arguments must be a number');
+class User{
+  constructor(name, surname){
+    this.name = name;
+    this.surname = surname;
   }
 
-  if(exp < 0){
-
-    throw new RangeError(`Exponent can't be  lass then 0`);
+  getFullName = () => { 
+    return `${this.name} ${this.surname}`;
   }
-
-
-  if(exp === 1){
-    return num;
-  }
-  
-  for(let i = 1; i < exp; i++){
-    result *= num;
-  }
-
-  return result;
 }
+
+class Worker {
+  constructor(name, rate, days){
+    this.name = name;
+    this.rate = rate;
+    this.days = days;
+  }
+
+  getSalary(){
+    return this.rate*this.days;
+  }
+}
+
+class Car{
+  constructor(brand, maxSpeed, currentSpeed = 0){
+    this.brand = brand;
+    this.maxSpeed = maxSpeed;
+    this.speed = currentSpeed;
+  }
+
+  stop = () =>{
+    this.speed = 0;
+    return `Speed of ${this.brand} is ${this.speed}`;
+  }
+
+  accelerate = (v) =>{
+    (this.speed + v <= this.maxSpeed) ?  this.speed += v :  this.speed = this.maxSpeed;
+  
+    return `Speed of ${this.brand} is ${this.speed}`;
+  }
+
+  deaccelerate = (v) => {
+    (this.speed - v > 0) ?  this.speed -= v :  this.speed = 0;
+  
+    return `Speed of ${this.brand} is ${this.speed}`;
+  }
+}
+
+const userName = ['Kirill', 'Monky', 'Bob', 'Fill'];
+const surname = ['Smith', 'Test', 'Foll', 'Grey'];
+
+const users = [];
+const workers = [];
+
+for(let i = 0; i < userName.length; i++){   
+    users.push(new User(userName[i], surname[i]));
+}
+
+for(let i = 0; i < userName.length; i++){   
+  workers.push(new Worker(userName[i], 4, 5));
+}
+
+console.log(users);
+console.log(workers);
+
+const car = new Car('Toyota Country', 260, 80);
+console.log(car.accelerate(100));
+console.log(car.accelerate(100));
+console.log(car.accelerate(100));
+console.log(car.deaccelerate(100));
+console.log(car.deaccelerate(80));
+console.log(car.stop());
+
 
