e7b10676765fc8f66c6142c09dc52bd1a55db838
ElyaDzhy <elmira.freshcode@gmail.com>
2023-03-29 11:20:15 +0300

    Add destructuring example

--- index.js
+++ index.js
@@ -1,87 +1,75 @@
 'use strict';
 
-class MyArray{
-  constructor(){
-    this.length = 0;
+const monitor = {
+  sizes: {
 
-    for(let i = 0; i < arguments.length; i++){
-      this[this.length] = arguments[i];
-      this.length++;
-  
+    height: {
+      value: 20,
+      scale: 'sm',
+    },
+
+    width: {
+      value: 20,
+      scale: 'sm',
+    },
+
+  },
+
+  brightness: '2000lm',
+
+  color: 'black',
+
+  manufacturer: {
+    name: 'Sony',
+
+    address: {
+      city: 'London',
+      street: 'Baker',
+      house: 27,
     }
-  }
- 
-
-  push = function(value){
-    this[this.length] = value;
-    this.length++;
-  }
-
-  pop = function(value) {
-    let result = this[this.length-1];
-    delete this[this.length-1];
-    this.length--;
-    return result;
-  }
-
-  forEach = function(callback){
-      for(let i = 0; i < this.length; i++){
-        callback(this[i]);
-
-      }
-  }
-
-  filter = function(callback){
-    const newarr = new MyArray();
-    for(let i = 0; i < this.length; i++){
-      callback(this[i]) ? newarr.push(this[i]) : false;
-
-    }
-    return newarr;
-  }
-
-  reverse = function(){
-    for (let i = this.length-1; i > 0; i--) {
-      let last = this[i];
-      let first = this[(this.length-1) - i];
-      this[(this.length-1) - i] = last;
-      this[i] = first;
-    }
-  }
-
-  reduce (fn, initialValue){
-    let accumulator = initialValue;
-    for(let i = 0; i < this.length; i++){
-      accumulator = fn(accumulator, this[i], i);
-    }
-
-    return accumulator;
-  }
-
+    ,
+  },
 
 }
 
-
-function sumAll(...args){
-  let sum = 0;
-  for(let  i = 0; i < args.length; i++){
-
-    if(Array.isArray(args[i])){
-      for(let j = 0; j < args[i].length; j++){
-          sum += args[i][j];
-      }
-      i++; 
-    }
-
-    sum += args[i];
+const cat = {
+  name: 'Barsik',
+  girlfriend: {
+    name: 'Lola',
+    childs: {
+        name: 'child1',
+    },
+   
   }
-
-  return sum;
 }
 
+//
+// const height = monitor.size.height.value;
+// const width = monitor.size.width.value;
 
-const arr = [1,2,3,4,5,6,6,5,2];
+const {sizes: {
+          height: {value: heightValue },
+          width: {value: widthValue}
+          },
 
-console.log(sumAll(arr,6,7));
+      manufacturer: {
+          address: {
+            city
+          }
+      }
+      } = monitor;
 
-console.log([1,3,5,7, ...[2,4,6,8]]);
+
+
+
+
+const {
+  girlfriend: {name: barsikGirl},
+  girlfriend:{childs: {name: child}}
+} = cat;
+
+
+
+// const diagonal = Math.sqrt((heightValue * heightValue) + (widthValue * widthValue));
+
+console.log(child, barsikGirl);
