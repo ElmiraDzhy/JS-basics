1994a22bccbe6d89745e7daf7ec1e0bfaa5633b6
ElyaDzhy <elmira.freshcode@gmail.com>
2023-03-29 09:30:11 +0300

    Power of arrowFunc

--- index.js
+++ index.js
@@ -1,113 +1,43 @@
 'use strict';
 
-const PI = 3.14;
+const newspaper = {
+  title: 'My super news',
+  articles: [
+    {
+      author: 'Jhon Doe',
+      header: 'Article1',
+      body: 'lorem ipsum',
 
-// CLASSES
+    },
+    {
+      author: 'Alex Doe',
+      header: 'Article2',
+      body: 'lorem yuefe de ',
 
-class Figure {
-  constructor(type){
-    this.type = type;
-  }
+    },
+    {
+      author: 'Jhon Smith',
+      header: 'Article3',
+      body: 'lorem frf 4fo34 wn gr',
 
-  getArea(){
-  }
+    },
+    {
+      author: 'Alan Doe',
+      header: 'Article4',
+      body: 'lorem inrf rfiuef ',
 
+    },
+    {
+      author: 'Mihael Doe',
+      header: 'Article5',
+      body: 'lorem dolor sit amet',
+
+    },
+  ],
+  showArticles(){
+    this.articles.forEach((currentArticle, index) => {
+      console.log(this.title);
+      console.log(`${index} : ${currentArticle.author} - ${currentArticle.header}` );
+    })
+  } 
 }
-
-class Triangle extends Figure{
-  constructor(a, h){
-    super('triangle');
-    this.a = a;
-    this.h = h;
-  
-  }
-
-  set a(v){
-    this._a = v;
-  }
-
-  get a(){
-    return this._a;
-  }
-
-  set h(v){
-    this._h = v;
-  }
-
-  get h(){
-    return this._h;
-  }
-
-
-
-  //переопределение родительского метода - OVERRIDE
-  getArea(){
-    return (this.a*this.h)/2;
-  }
-
-
-}
-
-class Square extends Figure{
-  constructor(a){
-    super('square');
-    this.a = a; 
-  }
-
-  set a(v){
-    this._a = v;
-  }
-
-  get a(){
-    return this._a;
-  }
-
-  getArea(){
-    return this.a * this.a;
-  }
-
-}
-
-class Circle extends Figure{
-  constructor(d){
-    super('circle');
-    this.d = d;
-
-  }
-
-  set d(v){
-    this._d = v;
-  }
-
-  get d(){
-    return this._d;
-  }
-
-  getArea(){
-    return PI * this.d;
-  }
-}
-
-const s = new Square(5);
-
-//
-/**
-
- * 
- * @param {Figure} obj 
- * @returns 
- */
-
-function getFugureArea(obj){
-  return obj.getArea();
-}
-
-/* 
-
-полиморфизм - способность функции работать с обьектами разных 
-типов если они связаны наследованием
-
-Так само как она работает с экземпляром Figure. она может работать 
-с любым наслдеником этоко класса
-
-*/
